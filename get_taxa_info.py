263563673f18a070f3d7d56cf50b41b574b9d49b
Graeme Fox <graeme.fox@manchester.ac.uk>
2017-06-05 15:07:22 +0100

    Some tidying up

--- get_taxa_info.py
+++ get_taxa_info.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python -tt
-
-#################
-# USAGE
-"""
-./get_taxa_info.py -i utax_taxonomy_output.tax -j ../ITS_S2SF3R/Sample_23-PW_ITS2_S2FS3R.fastq.fastaprimers_removed.fasta_otus/Sample_23.biom -k ../ITS_S2SF3R/Sample_23-PW_ITS2_S2FS3R.fastq.fastaprimers_removed.fasta_otus/new_refseqs.fna -o dummy_output.txt -r utax_sequence_file.fasta -p dummy_summary_out.txt -s 1500 -t ~/ncbi_nt/taxonomy/gi_taxid_nucl.dmp -u ~/ncbi_nt/taxonomy/names.dmp -v ~/ncbi_nt/taxonomy/nodes.dmp
-"""
-
 #FUNCTIONS
 
 def file_len(fname):
@@ -21,7 +14,6 @@ import os.path
 from Bio import SeqIO
 import subprocess
 import re
-
 # parse input arguments
 parser = argparse.ArgumentParser(description='pal_filter')
 parser.add_argument('-i','--input1', help='taxonomy file', \
@@ -97,27 +89,21 @@ with open(args.output, 'w') as output_file:
                 for row in dicts:
                     if row['ID'] == line.split("\t")[0]:
                         # for each line in the file it now attempts to get taxonomy info in 3 different ways:
-
                         ##################################
                         # OPTION 1  -  confidence level #1
                         ##################################
-
                         # if it has a taxonomy entry in the provided taxonomy file, then grab that:
                         success = "1"
                         rel_abund = 100*(float(line.split("\t")[1])/total)
                         seq = taxonomy_seq[line.split("\t")[0]]
                         sequence = seq.seq
-
                         # remove any stray newlines from the taxa line:
                         row['Taxa'] = row['Taxa'].replace("\n", "")
-
                         output_file.write(line.split("\t")[0] + "\t" + line.split("\t")[1].rstrip("\n") + "\t" + str(rel_abund) + "\t" + row['Taxa'].rstrip("\n") + "\t" + str(sequence) + "\n")
-
-                        # the taxa info from the provided taxa file is then used to construct the summary stats as this is the stuff we are most confident in
-
+                        # the taxa info from the provided taxa file is then used to construct
+                        #the summary stats as this is the stuff we are most confident in
                         # do all the taxa counting. tedious looping but it runs.
                         # if it does already have an entry in the counter, create one
-
                         if not row['Taxa'].split("; ")[0].lstrip("k__") in KINGDOM_COUNTER:
                             KINGDOM_COUNTER[row['Taxa'].split("; ")[0].lstrip("k__")] = 1
                         else:
@@ -149,7 +135,6 @@ with open(args.output, 'w') as output_file:
                 #################################
                 #OPTION 2  -  confidence level #2
                 #################################
-
                 # if there is no entry in the provided taxonomy file, it looks for a  100% match, at >75% of the alignment length to anything
                 # in the provided taxonomy file and assigns that taxonomy
                 if success == "0":
@@ -162,7 +147,7 @@ with open(args.output, 'w') as output_file:
                         temp_file.close
                     sub_string = "blastn -query temp_seq_file.fasta -db ~/Dropbox/QIIME/Latha/Graeme/utax_sequence_file.fasta -task blastn -dust no -outfmt \"7 qseqid sseqid evalue bitscore pident qcovhsp\" -perc_identity 100 -qcov_hsp_perc 75 -max_target_seqs 50 -num_threads 4 -out temp_blast_out.xml"
                     subprocess.call(sub_string, shell=True)
-                    #os.remove("temp_seq_file.fasta")
+                    os.remove("temp_seq_file.fasta")
                     blast_OTU_success = "0"
                     # if the blast search has found anything:
                     if os.path.isfile("temp_blast_out.xml"):
@@ -185,7 +170,7 @@ with open(args.output, 'w') as output_file:
                                                             perc_sim,
                                                             "Percent_alignment_coverage",
                                                             hsp_perc.rstrip("\n")])
-                                #os.remove("temp_blast_out.xml")
+                                os.remove("temp_blast_out.xml")
 
                         # write out all the data from stage II of the process
                     if blast_OTU_success == "1":
@@ -239,17 +224,8 @@ if os.path.isfile("temp_blast_out2.xml"):
                 else:
                     line = blast_output.next()
                     line = blast_output.next()
-
                         # parse the data
-    #                OTU_name = line.split("\t")[0]
                     ncbi_id = line.split("\t")[1]
-    #                e_val = line.split("\t")[2]
-    #                Bit = line.split("\t")[3]
-    #                perc_id = line.split("\t")[4]
-    #                perc_cov = line.split("\t")[5]
-    #                spec_name = line.split("\t")[6]
-    #                taxa_id = line.split("\t")[7]
-
                     blast_results.append('\t'.join([line.split("\t")[6],
                                                 ncbi_id.split("|")[1],
                                                 line.split("\t")[2],
@@ -257,16 +233,11 @@ if os.path.isfile("temp_blast_out2.xml"):
                                                 line.split("\t")[4],
                                                 line.split("\t")[5],
                                                 line.split("\t")[0]]) + "\n")
-    #os.remove("temp_blast_out2.xml")
-#blast_results looks like:
-# Avicennia officinalis 948275600 0	722	95.56	100 New.ReferenceOTU0
-
-
+    os.remove("temp_blast_out2.xml")
 
 data_present = []
 empty_data = []
 get_taxonomy = []
-
 with open("temp_taxo_file.txt", 'w') as taxo:
     for x, y in zip(list_of_data_to_blast, blast_results):
         if y == "No BLAST result found.":
@@ -287,15 +258,14 @@ with open("temp_taxo_file.txt", 'w') as taxo:
 
 ### write taxonomy IDs out into temporary file
 # required for the gb_taxonomy_tools packages to run correctly(https://github.com/spond/gb_taxonomy_tools)
-
 ## these can now be checked against the ncbi taxonomy database to retrieve the taxo info
 
 command_string = "gid-taxid temp_taxo_file.txt " + args.taxidnucl + " > gid-taxid_output.txt"
 subprocess.call(command_string, shell=True)
-#os.remove("temp_taxo_file.txt")
+os.remove("temp_taxo_file.txt")
 # the ids generated above can then be checked against the main database to get the long-format taxonomy info
-
-command_string = "cat gid-taxid_output.txt | taxonomy-reader " + args.names + " " + args.nodes + " > taxonomy_output.txt"
+command_string = "cat gid-taxid_output.txt | taxonomy-reader " + args.names, \
+                        + " " + args.nodes + " > taxonomy_output.txt"
 subprocess.call(command_string, shell=True)
 
 broken_taxonomy_output = []
@@ -306,9 +276,6 @@ if os.path.isfile("taxonomy_output.txt"):
             # see if there are any strings of 'n's in the taxonomy data
             # remove any that re found
             # this really destroys the output formatting if not done completely
-
-            #matchObj = re.match( r'\n\n', line)
-            #while matchObj:
             # if the taxonomy info is just a row full on N (some of them are)
             n_count = 0
             for x in item.split("\t"):
@@ -325,20 +292,17 @@ if os.path.isfile("taxonomy_output.txt"):
                 item = item.replace("\tn\tn", "\tn")
                 taxonomy_info.append(item)
 
-    #os.remove("taxonomy_output.txt")
+    os.remove("taxonomy_output.txt")
     output = []
     for item, row in zip(data_present, taxonomy_info):
-
         row = row.replace("\n", "")
-
-        print(row)
-        print(len(row.split("\t")))
-        #print(item, row)
         if row.split("\t")[3] == "No taxonomy information found" or len(row.split("\t")) < 16:
             broken_taxonomy_output.append('\t'.join([item.split("\t")[0],
                                     item.split("\t")[1],
                                     item.split("\t")[2],
-                                    "No taxonomy information found in Blast Database. Blast result gave species as " + item.split("\t")[3],
+                                    "No taxonomy information found in Blast , \
+                                        Database. Blast result gave species as ", \
+                                         + item.split("\t")[3],
                                     "Percent_similarity:",
                                     item.split("\t")[7].rstrip("\n"),
                                     "Percent_alignment_coverage:",
@@ -350,7 +314,12 @@ if os.path.isfile("taxonomy_output.txt"):
             output.append('\t'.join([item.split("\t")[0],
                                     item.split("\t")[1],
                                     item.split("\t")[2],
-                                    "k__" + row.split("\t")[5] + "; p__" + row.split("\t")[7] + "; c__" + row.split("\t")[9] + "; o__" + row.split("\t")[11] + "; f__" + row.split("\t")[13] + "; g__" + row.split("\t")[15],
+                                    "k__" + row.split("\t")[5] + "; p__", \
+                                        + row.split("\t")[7] + "; c__", \
+                                         + row.split("\t")[9] + "; o__", \
+                                          + row.split("\t")[11] + "; f__", \
+                                           + row.split("\t")[13] + "; g__", \
+                                            + row.split("\t")[15],
                                     "Percent_similarity",
                                     item.split("\t")[7],
                                     "Percent_alignment_coverage",
@@ -365,11 +334,6 @@ with open(args.output, 'a') as output_file:
         output_file.write(x)
     for x in empty_data:
         output_file.write(x + "\n")
-
-
-
-##### second round of collecting taxa counts
-# this time using the info from all 3 processes above
 
 KINGDOM_COUNTER2 = {}
 PHYLA_COUNTER2 = {}
@@ -392,9 +356,6 @@ with open(args.output, 'r') as input_file:
             matchObj1 = re.findall( r'No BLAST result found', taxa, re.M|re.I)
             matchObj2 = re.findall( r'No taxonomy information found in Blast Database', taxa, re.M|re.I)
             if (matchObj1 or matchObj2):
-
-
-            #if taxa == "No BLAST result found." or taxa == "No taxonomy information found in Blast Database.":
                 continue
             else:
                 if not taxa.split("; ")[0].lstrip("k__") in KINGDOM_COUNTER2:
@@ -430,7 +391,6 @@ with open(args.output, 'r') as input_file:
 with open(args.summary_output, 'w') as summary_out:
     summary_out.write("Top section of taxa info is derived only from the data that had entires in the utax taxonomy info. ie. from the closed ref OTU picking\n")
     summary_out.write("it does not include anything from the de-novo picking, although some (many) of those OTUs likely fall into the taxa listed here\n")
-
     summary_out.write("\nTAXA\tCOUNT\n")
     summary_out.write("\nKINGDOM\n")
     for x in KINGDOM_COUNTER:
@@ -480,9 +440,9 @@ with open(args.summary_output, 'w') as summary_out:
     #    summary_out.write(x + "\t" + str(SPECIES_COUNTER2[x]) + "\n")
 summary_out.close()
 # tidy up some files
-#if os.path.isfile("temp_blast_input.fasta"):
-    #os.remove("temp_blast_input.fasta")
-#if os.path.isfile("temp_grep_output.txt"):
-    #os.remove("temp_grep_output.txt")
-#if os.path.isfile("gid-taxmid_output.txt"):
-    #os.remove("gid-taxid_output.txt")
+if os.path.isfile("temp_blast_input.fasta"):
+    os.remove("temp_blast_input.fasta")
+if os.path.isfile("temp_grep_output.txt"):
+    os.remove("temp_grep_output.txt")
+if os.path.isfile("gid-taxmid_output.txt"):
+    os.remove("gid-taxid_output.txt")
